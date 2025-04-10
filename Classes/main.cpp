e3b75974e7edf54761f77de414358584b56c9f72
DragonFire-0 <115387324+DragonFire-0@users.noreply.github.com>
2025-04-10 19:03:26 +0000

    Wrote main pick for main loop

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -13,5 +13,32 @@
 using namespace std;
 
 int main() {
+    //Variable initilization
+    char controlChar = '';
+
+    //Welcome message
+    cout << "Welcome!" << endl
+    << "This program will help you manage your tasks for this Space Station." << endl;
     
+
+    //Main loop
+    while (controlChar != 'q'){
+        controlChar = mainDirec();
+
+    }
+}
+
+char mainDirec(){ 
+    cout << "Pick an option form below:" << endl << endl 
+    << "(a)Add a new task" << endl
+    << "(b)List tasks by name" << endl
+    << "(c)List tasks by Type" << endl
+    << "(d)Search by task name" << endl
+    << "(e)Remove tasks by index" << endl
+    << "(q)Quit" << endl;
+    
+    char controlChar = '';
+    cin >> controlChar;
+    cout << endl;
+    return controlChar
 }
